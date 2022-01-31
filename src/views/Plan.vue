e622332782d886903b6f4e2194852f4ee3e74359
Jona Löffler <jona.loeffler@online.de>
2022-01-31 17:08:36 +0100

    Add tailwind+daisyui

---
+++ src/views/Plan.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mx-auto">Erstelle deinen Essensplan</div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Home extends Vue {}
+</script>
