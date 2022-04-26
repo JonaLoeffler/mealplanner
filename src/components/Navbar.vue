568bcb329e66c619052975759b9fd47eee1777f4
Jona Löffler <jona.loeffler@online.de>
2022-04-26 18:49:10 +0200

    Update title

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -20,7 +20,7 @@
       </button>
     </div>
     <div class="flex-1 px-2 mx-2">
-      <span class="text-lg font-bold"> With two icons </span>
+      <span class="text-lg font-bold"> Meal Planner </span>
     </div>
     <div class="flex-none">
       <button class="btn btn-square btn-ghost">
